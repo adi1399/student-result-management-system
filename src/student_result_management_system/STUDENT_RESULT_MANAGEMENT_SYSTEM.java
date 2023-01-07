874a8febacea0203e0420ef89f274269252453a6
aditya <adityabhujbal1399@gmail.com>
2023-01-07 20:06:30 +0530

    first commit

---
+++ src/student_result_management_system/STUDENT_RESULT_MANAGEMENT_SYSTEM.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package student_result_management_system;
+
+/**
+ *
+ * @author adity
+ */
+public class STUDENT_RESULT_MANAGEMENT_SYSTEM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
